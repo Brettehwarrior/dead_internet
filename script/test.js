7ce2e7729dbaca1e322a2f16543ce1ea9373946a
Trent <trentthebaker@gmail.com>
2024-06-03 15:51:39 -0300

    Added Y users table

--- script/test.js
+++ script/test.js
@@ -1,19 +1,20 @@
 function createPostElement(post) {
-    const date = new Date(post.time);
+    const date = new Date(post.post_date);
     const postHTML = `
         <div class="post-user-container">
-            <img src="${'user.png'}" alt="A user's profile picture" class="post-user-pfp">
-            <span class="post-user-username">@${post.user_id}</span>
+            <img src="${post.user_pfp_directory}" alt="A user's profile picture" class="post-user-pfp">
+            <span class="post-user-username">@${post.user_display_name}</span>
+            <span class="post-user-username">@${post.user_handle}</span>
         </div>
-        <p class="post-content">${post.text}</p>
+        <p class="post-content">${post.post_content}</p>
         <div class="post-meta">
             <span class="post-meta-text">${date.toLocaleTimeString()}</span>
             <span class="post-meta-text">${date.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})}</span>
             <span class="post-meta-text">${'tinder for ipone'}</span>
         </div>
         <div class="post-metrics">
-            <span class="post-metrics-text"><i class="fa-solid fa-heart"></i> ${post.like_count}</span>
-            <span class="post-metrics-text"><i class="fa-solid fa-retweet"></i> ${'retweets'}</span>
+            <span class="post-metrics-text"><i class="fa-solid fa-heart"></i> ${post.post_like_count}</span>
+            <span class="post-metrics-text"><i class="fa-solid fa-retweet"></i> ${post.post_repost_count}</span>
         </div>
     `;
   
@@ -33,8 +34,13 @@ window.initSqlJs().then(function (SQL) {
 
         const db = new SQL.Database(new Uint8Array(buffer));
 
-        const preparedStatement = db.prepare("SELECT * FROM posts WHERE id=:user_id");
-        const post = preparedStatement.getAsObject({":user_id":1});
+        const preparedStatement = db.prepare(`
+            SELECT posts.*, users.*
+            FROM posts
+            JOIN users
+            WHERE posts.post_id = :post_id
+        `);
+        const post = preparedStatement.getAsObject({":post_id":1});
         console.log(post);
 
         const postDiv = createPostElement(post);
