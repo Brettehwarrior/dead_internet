45e26781b1e923460535f928d2ae2c9432db1d3c
Trent <trentthebaker@gmail.com>
2024-06-17 17:47:03 -0300

    we're so back

--- script/news.js
+++ script/news.js
@@ -1,28 +1,3 @@
-async function loadWordList() {
-    const response = await fetch("eng-ca_web_2020_10K-words_pruned.txt");
-    if (!response.ok)
-        throw new Error("Could not fetch text file ");
-
-    const text = await response.text();
-    const lines = text.trim().split("\n");
-    const wordList = [];
-
-    lines.forEach(line => {
-        const line_data = line.trim().split("\t");
-        if (line_data.length === 3) {
-            const word = line_data[1];
-
-            if (!word.includes(" ") && !/[^a-zA-Z]/.test(word))
-            {
-                const frequency = parseInt(line_data[2]);
-                // This will lead to a large list, but we suffer for fashion
-                for (let i = 0; i < frequency; i++)
-                    wordList.push(word)
-            }
-        }
-    });
-    return wordList;
-}
 
 function getRandomDarkColor() {
     var color = '#';
@@ -30,35 +5,6 @@ function getRandomDarkColor() {
         color += Math.floor(Math.random() * 10);
     }
     return color;
-}
-
-function getRandomEmoji() {
-    const emojiCategories = [
-        { start: 0x1F300, end: 0x1F3FA },   
-        { start: 0x1F400, end: 0x1F4FD },   
-        //{ start: 0x1F600, end: 0x1F64F },   // Mostly faces
-        { start: 0x1F680, end: 0x1F6C5 },   
-        { start: 0x1F910, end: 0x1F96B },   
-        { start: 0x1F980, end: 0x1F9D9 }    
-    ];
-
-    function getRandomEmojiInRange(start, end) {
-        const range = end - start + 1;
-        const codePoint = start + Math.floor(Math.random() * range);
-        return String.fromCodePoint(codePoint);
-    }
-
-    const randomCategory = emojiCategories[Math.floor(Math.random() * emojiCategories.length)];
-    return getRandomEmojiInRange(randomCategory.start, randomCategory.end);
-}
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function getRandomPunctuation() {
-    const punctuation = [".",".",".",".",".",".",".",".","?","?","!",];
-    return punctuation[Math.floor(Math.random() * punctuation.length)];
 }
 
 function updateFavicon(emoji) {
--- script/y.js
+++ script/y.js
@@ -11,47 +11,6 @@ function handleScroll(callback) {
     }
 }
 
-function createPostElement(post) {
-    const date = new Date(post.post_date);
-    const verifiedHtml = (post.user_verified != 1) ? "" : '<img class="post-user-verified-badge" src="img/patch-check-fill.svg" alt="Verified Badge">';
-
-    let postEmbedContent = "";
-    if (post.post_image_url) {
-        postEmbedContent = `
-            <div class="post-media-container">
-                <img class="post-media-image" src="${post.post_image_url}" alt="Post image">
-            </div>`;
-    }
-
-    const postHTML = `
-        <div class="post-user-container">
-            <img src="${post.user_pfp_directory}" alt="A user's profile picture" class="post-user-pfp">
-            ${verifiedHtml}
-            <span class="post-user-display-name">${post.user_display_name}</span>
-            <span class="post-user-handle">@${post.user_handle}</span>
-        </div>
-        <p class="post-content">${post.post_content}</p>
-
-        ${postEmbedContent}
-
-        <div class="post-meta">
-            <span class="post-meta-text">${date.toLocaleTimeString('en-us', {timeStyle:'short'})}</span>
-            <span class="post-meta-text">${date.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})}</span>
-            <span class="post-meta-text">${post.post_source}</span>
-        </div>
-        <div class="post-metrics">
-            <a href="signup.html" class="post-metrics-text"><i class="fa-solid fa-heart"></i> ${post.post_like_count}</span></a>
-            <a href="signup.html" class="post-metrics-text"><i class="fa-solid fa-retweet"></i> ${post.post_repost_count}</span></a>
-        </div>
-    `;
-  
-    const container = document.createElement('div');
-    container.innerHTML = postHTML;
-    container.className = "post";
-  
-    return container;
-}
-
 window.initSqlJs().then(function (SQL) {
     console.log(SQL);
 
@@ -77,19 +36,103 @@ window.initSqlJs().then(function (SQL) {
             WHERE posts.post_id = :post_id
         `);
 
-        // Get random posts
-        const appendRandomPost = () => {
-            let postIndex = Math.floor(Math.random() * postCount) + 1;
-            const post = stmtGetPost.getAsObject({":post_id": postIndex});
-            const postDiv = createPostElement(post);
-            document.getElementById("posts").appendChild(postDiv);
-        }
+        ////////// Load random words and start populating
+        loadWordList().then(wordList => {
+            function getRandomWord() {
+                return wordList[Math.floor(Math.random() * wordList.length)];
+            }
+        
+            function makeSentence(punctuation=true) {
+                const wordCount = 6 + Math.floor(Math.random() * 9);
+                let sentence = capitalizeFirstLetter(getRandomWord());
+                for (let i = 0; i < wordCount-1; i++)
+                {
+                    sentence += " " + getRandomWord();
+                }
+                if (punctuation)
+                    sentence += getRandomPunctuation();
+                return sentence;
+            }
+        
+            function makeParagraph() {
+                const sentenceCount = 1 + Math.floor(Math.random() * 4);
+                let paragraph = makeSentence();
+                for (let i = 0; i < sentenceCount-1; i++)
+                {
+                    paragraph += " " + makeSentence();
+                }
+                return paragraph;
+            }
 
-        // Populate initally
-        for (let i = 0; i < 6; i++)
-            appendRandomPost();
+            function createPostElement(post) {
+                const date = new Date(post.post_date);
+                const verifiedHtml = (post.user_verified != 1) ? "" : '<img class="post-user-verified-badge" src="img/patch-check-fill.svg" alt="Verified Badge">';
 
-        // Add new post on scroll to bottom
-        window.addEventListener("scroll", ()=>{handleScroll(appendRandomPost);});
+                let postEmbedContent = "";
+                if (post.post_image_url) {
+                    postEmbedContent = `
+                        <div class="post-media-container">
+                            <img class="post-media-image" src="${post.post_image_url}" alt="Post image">
+                        </div>`;
+                } else if (post.post_embed_content) {
+                    postEmbedContent = `
+                    <div class="post-media-container">
+                        <a class="post-embed" href="news.html" target="_blank">
+                            <span><i class="fa-solid fa-up-right-from-square"></i> ${post.post_embed_content}</span>
+                            <p class="post-meta-text">trustworthynews</p>
+                        </a>
+                    </div>`;
+                }
+
+                let postContent = post.post_content;
+                if (postContent == "[RANDOM]") {
+                    postContent = makeParagraph();
+                }
+
+                const postHTML = `
+                    <div class="post-user-container">
+                        <img src="${post.user_pfp_directory}" alt="A user's profile picture" class="post-user-pfp">
+                        <span class="post-user-display-name">${post.user_display_name}</span>
+                        ${verifiedHtml}
+                        <span class="post-user-handle">@${post.user_handle}</span>
+                    </div>
+                    <p class="post-content">${postContent}</p>
+
+                    ${postEmbedContent}
+
+                    <div class="post-meta">
+                        <span class="post-meta-text">${date.toLocaleTimeString('en-us', {timeStyle:'short'})}</span>
+                        <span class="post-meta-text">${date.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"})}</span>
+                        <span class="post-meta-text">${post.post_source}</span>
+                    </div>
+                    <div class="post-metrics">
+                        <a href="signup.html" class="post-metrics-text"><i class="fa-solid fa-heart"></i> ${post.post_like_count}</span></a>
+                        <a href="signup.html" class="post-metrics-text"><i class="fa-solid fa-retweet"></i> ${post.post_repost_count}</span></a>
+                    </div>
+                `;
+            
+                const container = document.createElement('div');
+                container.innerHTML = postHTML;
+                container.className = "post";
+            
+                return container;
+            }
+
+
+            // Get random posts
+            const appendRandomPost = () => {
+                let postIndex = Math.floor(Math.random() * postCount) + 1;
+                const post = stmtGetPost.getAsObject({":post_id": postIndex});
+                const postDiv = createPostElement(post);
+                document.getElementById("posts").appendChild(postDiv);
+            }
+    
+            // Populate initally
+            for (let i = 0; i < 6; i++)
+                appendRandomPost();
+    
+            // Add new post on scroll to bottom
+            window.addEventListener("scroll", ()=>{handleScroll(appendRandomPost);});
+        });
     })();
 });
